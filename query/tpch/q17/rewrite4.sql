3607fd57c9ab1b384fb2926b144ae74241de4e9d
Nan Champion <cbn@Nans-MacBook-Pro.local>
2024-02-28 10:57:01 +0800

    Finish adb4 1T

--- query/tpch/q17/rewrite4.sql
+++ query/tpch/q17/rewrite4.sql
@@ -1,5 +1,4 @@
 create or replace view aggView922865157967870597 as select l_partkey as v17, SUM(l_extendedprice) as v28, l_quantity as v5 from lineitem as lineitem group by l_partkey,l_quantity;
 create or replace view aggJoin5083673775524916742 as select v1_partkey as v17, v28 from q17_inner as q17_inner, aggView922865157967870597 where q17_inner.v1_partkey=aggView922865157967870597.v17 and v5>v1_quantity_avg;
 create or replace view aggView8989783677004721760 as select v17, SUM(v28) as v28 from aggJoin5083673775524916742 group by v17;
-create or replace view aggJoin5064114370911071471 as select v28 from part as part, aggView8989783677004721760 where part.p_partkey=aggView8989783677004721760.v17 and p_brand= 'Brand#23' and p_container= 'MED BOX';
-select (SUM(v28) / 7.0) as v29 from aggJoin5064114370911071471;
+select (SUM(v28) / 7.0) as v29 from part as part, aggView8989783677004721760 where part.p_partkey=aggView8989783677004721760.v17 and p_brand= 'Brand#23' and p_container= 'MED BOX';
