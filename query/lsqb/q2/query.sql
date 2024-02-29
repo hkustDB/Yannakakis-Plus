d47812c3876eca6c10a5a8a2908b81a911a44ae2
Nan Champion <cbn@Nans-MacBook-Pro.local>
2024-02-29 19:29:49 +0800

    ADB rewrite version1.0

--- query/lsqb/q2/query.sql
+++ query/lsqb/q2/query.sql
@@ -1,5 +1,5 @@
-COPY (SELECT count(*)
+SELECT count(*)
 FROM Person_knows_Person, Comment, Post
 WHERE Person_knows_Person.Person1Id = Comment.hasCreator_PersonId
 	AND Person_knows_Person.Person2Id = Post.hasCreator_PersonId
-	AND Comment.replyOf_PostId = Post.PostId ) TO '/dev/null' (DELIMITER ',');
+	AND Comment.replyOf_PostId = Post.PostId
