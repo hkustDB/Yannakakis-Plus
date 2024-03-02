be789617e6178977a3d88406be034339ec085c66
Nan Champion <cbn@Nans-MacBook-Pro.local>
2024-03-02 21:35:06 +0800

    Add preliminary report PK optimize support

--- query/extra/q2/query.sql
+++ query/extra/q2/query.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM `Comment`, Comment_hasTag_Tag, Tag
 WHERE Comment_hasTag_Tag.CommentId = `Comment`.CommentId
-	AND Tag.TagId = Comment_hasTag_Tag.TagId AND `Comment`.replyOf_CommentId < Tag.TagId
+	AND Tag.TagId = Comment_hasTag_Tag.TagId AND `Comment`.replyOf_CommentId < Tag.hasType_TagClassId
