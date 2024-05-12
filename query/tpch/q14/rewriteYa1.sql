b19cce4a3ee854dfa2422077e2cfe9b9bb9bc150
ChampionNan <bchenba@connect.ust.hk>
2024-05-12 20:31:32 +0800

    Add missing json

--- query/tpch/q14/rewriteYa1.sql
+++
@@ -1,6 +0,0 @@
-create or replace view semiUp7177333605625820582 as select p_partkey as v2, p_type as v21 from part AS part where (p_partkey) in (select l_partkey from lineitem AS lineitem where l_shipdate>=DATE '1995-09-01' and l_shipdate<DATE '1995-10-01');
-create or replace view semiDown3916612593589395738 as select l_partkey as v2, l_extendedprice as v6, l_discount as v7 from lineitem AS lineitem where (l_partkey) in (select v2 from semiUp7177333605625820582) and l_shipdate>=DATE '1995-09-01' and l_shipdate<DATE '1995-10-01';
-create or replace view aggView4153119450768530222 as select v2, v6 * (1 - v7) as caseRes, SUM(v6 * (1 - v7)) as v29, COUNT(*) as annot from semiDown3916612593589395738 group by v2,caseRes;
-create or replace view aggJoin2512438082076209224 as select v21, caseRes, v29, annot from semiUp7177333605625820582 join aggView4153119450768530222 using(v2);
-create or replace view res as select ((100.0 * SUM( CASE WHEN v21 LIKE 'PROMO%' THEN caseRes * annot ELSE 0.0 END)) / SUM(v29)) as v30 from aggJoin2512438082076209224;
-select sum(v30) from res;
--- query/tpch/q19/rewriteYa1.sql
+++
@@ -1,6 +0,0 @@
-create or replace view semiUp171388673558521313 as select l_partkey as v17, l_extendedprice as v6, l_discount as v7 from lineitem AS lineitem where (l_partkey) in (select p_partkey from part AS part where p_brand= 'Brand#34' and p_size>=1 and p_container IN ('LG CASE','LG BOX','LG PACK','LG PKG') and p_size<=15) and l_quantity>=21 and l_shipinstruct= 'DELIVER IN PERSON' and l_quantity<=21 + 10 and l_shipmode IN ('AIR','AIR REG');
-create or replace view semiDown471191034622720660 as select p_partkey as v17 from part AS part where (p_partkey) in (select v17 from semiUp171388673558521313) and p_brand= 'Brand#34' and p_size>=1 and p_container IN ('LG CASE','LG BOX','LG PACK','LG PKG') and p_size<=15;
-create or replace view aggView937446625105840731 as select v17 from semiDown471191034622720660;
-create or replace view aggJoin6064087226286578676 as select v6, v7 from semiUp171388673558521313 join aggView937446625105840731 using(v17);
-create or replace view res as select SUM((v6 * (1 - v7))) as v27 from aggJoin6064087226286578676;
-select sum(v27) from res;
